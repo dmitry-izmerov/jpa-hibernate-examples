df54d7346b2661477de5452f128de44485e0f4d2
demi <idd90i@gmail.com>
2018-09-15 17:18:49 +0300

    start example with persistence.xml

---
+++ java-persistence-with-hibernate-20/src/main/java/ru/demi/App.java
@@ -0,0 +1,49 @@
+package ru.demi;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import ru.demi.model.Message;
+import ru.demi.util.HibernateUtil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+public class App {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = null;
+        EntityManager entityManager = null;
+        EntityTransaction tx = null;
+
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("hiberApp");
+            entityManager = entityManagerFactory.createEntityManager();
+            tx = entityManager.getTransaction();
+            tx.begin();
+
+            List<Message> messages = entityManager.createQuery("select m from Message m", Message.class)
+                .getResultList();
+            log.info(messages.get(0).getText());
+
+            messages.get(0).setText("text was changed!");
+
+            tx.commit();
+        } catch (Exception e) {
+            if (Objects.nonNull(tx)) {
+                tx.rollback();
+            }
+            throw new RuntimeException(e);
+        } finally {
+            if (Objects.nonNull(entityManager)) {
+                entityManager.close();
+            }
+            if (Objects.nonNull(entityManagerFactory)) {
+                entityManagerFactory.close();
+            }
+        }
+    }
+}
---
+++ java-persistence-with-hibernate-20/src/main/java/ru/demi/util/HibernateUtil.java
@@ -0,0 +1,27 @@
+package ru.demi.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+@Slf4j
+public class HibernateUtil {
+
+	private static final SessionFactory sessionFactory = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() {
+		try {
+            Configuration config = new Configuration().configure();
+            //config.setImplicitNamingStrategy(ImplicitNamingStrategyImpl.INSTANCE);
+            //config.setPhysicalNamingStrategy(PhysicalNamingStrategyImpl.INSTANCE);
+            return config.buildSessionFactory();
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+			throw new RuntimeException("There was an error building the factory");
+		}
+	}
+	
+	public static SessionFactory getSessionFactory(){
+		return sessionFactory;
+	}
+}
