4956a0e058ff74cd50aab8ada5b1eaeffe9b4ac8
demi <idd90i@gmail.com>
2018-09-17 22:41:52 +0300

    added id field for full picture

--- java-persistence-with-hibernate/src/main/java/ru/demi/model/Item.java
+++ java-persistence-with-hibernate/src/main/java/ru/demi/model/Item.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -15,6 +17,11 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Item {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
     @NotNull
     @Size(
         min = 2,
